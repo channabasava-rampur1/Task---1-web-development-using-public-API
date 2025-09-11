6b8fc0e78ce3de2bf8b6e1e253838c9cac5e1f5b
Channabasava Rampur <channabasavar9@gmail.com>
2025-09-11 22:54:19 +0530

    configuring API and fetching response

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, jsonify
+import google.generativeai as genai
+
+# Initialize Flask
+app = Flask(__name__)
+
+# Configure Gemini API Key
+genai.configure(api_key="AIzaSyCwTJCv2dGuOhCrVKdg5Sdnj3u-Kh98I3w")
+
+# Model
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/ask", methods=["POST"])
+def ask():
+    user_query = request.json.get("query")
+    response = model.generate_content(user_query)
+    return jsonify({"answer": response.text})
+
+if __name__ == "__main__":
+    app.run(debug=True)
